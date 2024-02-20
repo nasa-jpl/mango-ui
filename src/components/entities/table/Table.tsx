9a80e06489ab55c870d53e99b66b0b92697b41de
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-02-19 19:24:19 -0800

    implemented global/page time range

--- src/components/entities/table/Table.tsx
+++ src/components/entities/table/Table.tsx
@@ -1,3 +1,4 @@
+import { DateRange } from "../../../types/time";
 import { TableEntity } from "../../../types/view";
 import EntityHeader from "../../page/EntityHeader";
 import DataGrid from "../../ui/DataGrid/DataGrid";
@@ -5,9 +6,10 @@ import "./Table.css";
 
 export declare type TableProps<T> = {
   tableEntity: TableEntity<T>;
+  pageDateRange: DateRange;
 };
 
-export function Table<T>({ tableEntity }: TableProps<T>) {
+export function Table<T>({ tableEntity, pageDateRange }: TableProps<T>) {
   const columnDefs = tableEntity.fields.map((field) => ({
     field,
     filter: "string",
