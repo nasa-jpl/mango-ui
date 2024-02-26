f470a0f932fdd7007fffe8fd02d4830fd6c8d211
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-02-26 14:20:48 -0800

    fix

--- src/components/entities/table/Table.tsx
+++ src/components/entities/table/Table.tsx
@@ -6,17 +6,10 @@ import "./Table.css";
 
 export declare type TableProps<T> = {
   tableEntity: TableEntity<T>;
-<<<<<<< HEAD
-  pageDateRange: DateRange;
-};
-
-export function Table<T>({ tableEntity, pageDateRange }: TableProps<T>) {
-=======
   dateRange: DateRange;
 };
 
 export function Table<T>({ tableEntity, dateRange }: TableProps<T>) {
->>>>>>> 7c6ca9e6b0678724ed8a355e422a275a78ec4d25
   const columnDefs = tableEntity.fields.map((field) => ({
     field,
     filter: "string",
