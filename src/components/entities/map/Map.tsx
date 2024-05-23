5b17c2b9e68d6dd0c68237ab2a24df1d816283c9
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-05-23 09:42:20 -0700

    removed cesium ion references | changed basemap

--- src/components/entities/map/Map.tsx
+++ src/components/entities/map/Map.tsx
@@ -1,4 +1,9 @@
-import { Viewer as CesiumViewer } from "cesium";
+import {
+  Viewer as CesiumViewer,
+  Ion,
+  ProviderViewModel,
+  UrlTemplateImageryProvider,
+} from "cesium";
 import { useEffect, useRef } from "react";
 import { DateRange } from "../../../types/time";
 import { MapEntity } from "../../../types/view";
@@ -12,13 +17,37 @@ export declare type MapProps = {
 
 export const Map = ({ mapEntity /* dateRange */ }: MapProps) => {
   const map = useRef<HTMLDivElement>(null);
+  const imageryViewModels: ProviderViewModel[] = [];
+  Ion.defaultAccessToken = "";
+
+  imageryViewModels.push(
+    new ProviderViewModel({
+      name: "BlueMarble_ShadedRelief_Bathymetry",
+      tooltip: "BlueMarble_ShadedRelief_Bathymetry",
+      iconUrl:
+        "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/BlueMarble_ShadedRelief_Bathymetry/default/2004-08/500m/2/1/1.jpeg",
+      creationFunction: function () {
+        return new UrlTemplateImageryProvider({
+          url: "https://gibs-{s}.earthdata.nasa.gov/wmts/epsg4326/best/BlueMarble_ShadedRelief_Bathymetry/default/2004-08/500m/{z}/{y}/{x}.jpeg",
+          minimumLevel: 0,
+          maximumLevel: 18,
+        });
+      },
+    })
+  );
 
   useEffect(() => {
-    new CesiumViewer(map.current as HTMLDivElement, {
+    const viewer = new CesiumViewer(map.current as HTMLDivElement, {
+      imageryProviderViewModels: imageryViewModels,
+      selectedImageryProviderViewModel: imageryViewModels[1],
       timeline: false,
       animation: false,
       fullscreenButton: false,
+      infoBox: false,
     });
+
+    viewer.baseLayerPicker.viewModel.terrainProviderViewModels = [];
+    viewer.cesiumWidget.creditContainer.remove();
   }, []);
 
   return (
