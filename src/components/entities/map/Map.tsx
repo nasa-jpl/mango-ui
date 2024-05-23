352a4df6bf05a48b623df4c477408dd918ff435f
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-05-23 10:05:20 -0700

    refactor

--- src/components/entities/map/Map.tsx
+++ src/components/entities/map/Map.tsx
@@ -17,38 +17,40 @@ export declare type MapProps = {
 
 export const Map = ({ mapEntity /* dateRange */ }: MapProps) => {
   const map = useRef<HTMLDivElement>(null);
-  const imageryViewModels: ProviderViewModel[] = [];
   Ion.defaultAccessToken = "";
 
-  imageryViewModels.push(
-    new ProviderViewModel({
-      name: "BlueMarble_ShadedRelief_Bathymetry",
-      tooltip: "BlueMarble_ShadedRelief_Bathymetry",
-      iconUrl:
-        "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/BlueMarble_ShadedRelief_Bathymetry/default/2004-08/500m/2/1/1.jpeg",
-      creationFunction: function () {
-        return new UrlTemplateImageryProvider({
-          url: "https://gibs-{s}.earthdata.nasa.gov/wmts/epsg4326/best/BlueMarble_ShadedRelief_Bathymetry/default/2004-08/500m/{z}/{y}/{x}.jpeg",
-          minimumLevel: 0,
-          maximumLevel: 18,
-        });
-      },
-    })
-  );
-
   useEffect(() => {
+    // Define basemaps
+    const imageryViewModels: ProviderViewModel[] = [];
+    imageryViewModels.push(
+      new ProviderViewModel({
+        name: "BlueMarble_ShadedRelief_Bathymetry",
+        tooltip: "BlueMarble_ShadedRelief_Bathymetry",
+        iconUrl:
+          "https://gibs.earthdata.nasa.gov/wmts/epsg4326/best/BlueMarble_ShadedRelief_Bathymetry/default/2004-08/500m/2/1/1.jpeg",
+        creationFunction: function () {
+          return new UrlTemplateImageryProvider({
+            url: "https://gibs-{s}.earthdata.nasa.gov/wmts/epsg4326/best/BlueMarble_ShadedRelief_Bathymetry/default/2004-08/500m/{z}/{y}/{x}.jpeg",
+            minimumLevel: 0,
+            maximumLevel: 18,
+          });
+        },
+      })
+    );
+
+    // Initialize map viewer
     const viewer = new CesiumViewer(map.current as HTMLDivElement, {
       imageryProviderViewModels: imageryViewModels,
       selectedImageryProviderViewModel: imageryViewModels[1],
       timeline: false,
       animation: false,
       fullscreenButton: false,
-      infoBox: false,
     });
 
+    // Remove cesium-ion stuff
     viewer.baseLayerPicker.viewModel.terrainProviderViewModels = [];
     viewer.cesiumWidget.creditContainer.remove();
-  }, [imageryViewModels]);
+  }, []);
 
   return (
     <>
