e8e3aa3d3eb765dd35a4758cfa3a601beca12531
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2024-10-01 14:39:44 -0700

    ignore https errors for playwright testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,6 +36,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://localhost:5174",
 
+    ignoreHTTPSErrors: true,
+
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
