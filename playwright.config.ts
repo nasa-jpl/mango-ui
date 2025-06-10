51792522285c624a9e33c51547743fe0cb4df398
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2025-06-10 09:47:50 -0700

    updated playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,10 +17,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Capture trace */
-  use: {
-    trace: "on-first-retry", // record traces on first retry of each test
-  },
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
