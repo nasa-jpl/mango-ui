7ac39500efeff53e8e1b18d9865781455781a6d2
Aaron Plave <aaron.plave@jpl.nasa.gov>
2025-06-10 16:44:48 -0700

    Fix

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -119,7 +119,7 @@ export async function saveView(view: View) {
   const url =
     config.endpoints.data +
     config.api.data.jsonStore
-      .replace("{METHOsD}", "store")
+      .replace("{METHOD}", "store")
       .replace("{KEY}", "default-view");
 
   const response = await fetch(url, {
