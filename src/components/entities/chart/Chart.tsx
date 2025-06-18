02021dca8346cfe9ac6f535d78ea213d229c59a8
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2025-06-18 09:28:10 -0700

    made time format consistent across downsampling factors

--- src/components/entities/chart/Chart.tsx
+++ src/components/entities/chart/Chart.tsx
@@ -1038,9 +1038,9 @@ export const Chart = ({
             time: {
               displayFormats: {
                 millisecond: "HH:mm:ss.SSS",
-                second: "HH:mm:ss",
-                minute: "HH:mm",
-                hour: "HH",
+                second: "HH:mm:ss.SSS",
+                minute: "HH:mm:ss.SSS",
+                hour: "HH:mm:ss.SSS",
               },
             },
             ticks: {
