4f2ba8bc5573ddddc67166523eed3d38fdb27a8a
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2025-06-23 15:53:36 -0700

    test

--- e2e-tests/tests/Navigation.spec.ts
+++ e2e-tests/tests/Navigation.spec.ts
@@ -11,6 +11,10 @@ test.beforeEach(async ({ page }) => {
 // });
 
 test("navigates to page", async ({ page }) => {
+  page.on("requestfailed", (request) => {
+    console.log("FAILED:", request.url(), ":", request.failure()?.errorText);
+  });
+
   page.on("response", (response) => {
     if (response.status() >= 400) {
       console.log(`ERROR ${response.status()}: ${response.url()}`);
