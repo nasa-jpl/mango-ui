2ffa617736f3a27f13c4a480f5207be694ae5f8a
Marjorie Lucas <marjorie.j.lucas@jpl.nasa.gov>
2025-07-31 16:53:28 -0700

    fix timestamp merging

--- src/components/entities/table/Table.tsx
+++ src/components/entities/table/Table.tsx
@@ -299,9 +299,13 @@ const Table = memo(function Table({
       flex: 1,
       resizable: true,
       sortable: true,
-      valueGetter: (params) => {
-        const rowData = params.data;
-        return rowData["timestamp"] ?? null;
+      valueGetter: (
+        params: ValueGetterParams<Record<string, DataResponseDataEntry>>
+      ) => {
+        if (!params.data) {
+          return "";
+        }
+        return Object.values(params.data)[0].timestamp ?? "";
       },
       valueFormatter: (params) => {
         return params.value.split("+")[0];
@@ -468,9 +472,7 @@ const Table = memo(function Table({
       finalResults = results;
     }
 
-    // Iterate over all layers to retrieve unique timestamps
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const timestampMap = new Map<string, any[]>();
+    const rows: Record<string, ProcessedDataResponseDataEntry>[] = [];
 
     finalResults.forEach(({ layer, result }) => {
       const metadataCache: Record<string, ProductField> = {};
@@ -486,7 +488,7 @@ const Table = memo(function Table({
           }
         });
       }
-      result.data.forEach((result) => {
+      result.data.forEach((result, i) => {
         const processedResult: ProcessedDataResponseDataEntry = result;
         Object.keys(result).forEach((key) => {
           // Compute thresholds for result if metadata available for the field
@@ -495,26 +497,13 @@ const Table = memo(function Table({
             processedResult[key]._thresholds = thresholds;
           }
         });
-        const timestampEntry = timestampMap.get(result.timestamp) || [];
-        timestampEntry.push({ [layer.id]: processedResult });
-        timestampMap.set(result.timestamp, timestampEntry);
+        if (!rows[i]) {
+          rows[i] = { [layer.id]: processedResult };
+        } else {
+          rows[i] = { ...rows[i], [layer.id]: processedResult };
+        }
       });
     });
-
-    // Sort down chronologically
-    const sortedTimestampMap = new Map(
-      [...timestampMap.entries()].sort(
-        (a, b) => new Date(a[0]).getTime() - new Date(b[0]).getTime()
-      )
-    );
-
-    // Merge entries with the same timestamp
-    const rows: Record<string, DataResponseDataEntry>[] = Array.from(
-      sortedTimestampMap.entries()
-    ).map(([timestamp, objects]) => ({
-      timestamp,
-      ...objects.reduce((acc, obj) => ({ ...acc, ...obj }), {}),
-    }));
 
     setRowData(rows);
   };
